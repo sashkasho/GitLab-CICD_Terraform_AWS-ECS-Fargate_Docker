dc5bc73aa6cb3c8363f0eee3e3be7b478533ed32
Alexandra Sholudko <shol.alexandra@gmail.com>
2022-11-30 12:29:34 +0000

    Update output.tf

--- tf-infra/modules/network/output.tf
+++ tf-infra/modules/network/output.tf
@@ -10,6 +10,9 @@ output "public_subnets_id" {
   value = aws_subnet.public_subnet.*.id
 }
 
+output "private_subnets_id" {
+  value = aws_subnet.private_subnet.*.id
+}
 /* output "private_subnets_id" {
   value = ["${aws_subnet.private_subnet.*.id}"]
 } */
